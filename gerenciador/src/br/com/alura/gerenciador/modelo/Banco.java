01b8b5d492e441d98e84450b4c511142d7663fba
DerickLFAndrade <derickspy15@gmail.com>
2023-01-13 07:55:03 -0300

    início de autenticação via login simulado

--- gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -8,12 +8,20 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> listEmpresa = new ArrayList<>();
+	private static List<Usuario> listUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	static {
 		  Empresa empresa = new Empresa(chaveSequencial++, "Alura", new Date());
 		  Empresa empresa2 = new Empresa(chaveSequencial++, "Caelum", new Date());
 		  Banco.listEmpresa.add(empresa);
 		  Banco.listEmpresa.add(empresa2);
+		  
+		  Usuario user1 = new Usuario("Derick", "senha123");
+		  Usuario user2 = new Usuario("Lucas", "senha1234");
+		  
+		  Banco.listUsuarios.add(user1);
+		  Banco.listUsuarios.add(user2);
+		  
 		}
 	
 	public static List<Empresa> getListEmpresa() {
